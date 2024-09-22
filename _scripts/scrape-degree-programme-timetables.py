5c6fedaceab5994b2fe21c42d181fc319251ba2f
matypist <69389461+matypist@users.noreply.github.com>
2024-09-22 03:14:39 +0200

    Hard-code CS schedules for the first week of lessons

--- _scripts/scrape-degree-programme-timetables.py
+++ _scripts/scrape-degree-programme-timetables.py
@@ -51,8 +51,8 @@ def parse(DOM):
 
         # Hard-coded list of (course_code, channel, teacher_name) erroneous combinations to be ignored
         ignore_conditions = [
-            ("1015883", "1", "MASI IACOPO"),  # Ignore MASI IACOPO's class for course 1015883 on channel 1
-            ("1020420", "1", "PIPERNO ADOLFO")  # Ignore PIPERNO ADOLFO's class for course 1020420 on channel 1
+            # ("1015883", "1", "MASI IACOPO"),  # Ignore MASI IACOPO's class for course 1015883 on channel 1
+            # ("1020420", "1", "PIPERNO ADOLFO")  # Ignore PIPERNO ADOLFO's class for course 1020420 on channel 1
         ]
 
         for h3 in div.find_all('h3'):
@@ -438,6 +438,95 @@ if __name__ == '__main__':
     # Save the timetables to a JSON file
     with open(f"../data/timetables_raw_{degree_programme_code}_{academic_year.replace('/', '-')}.json", 'w') as rawTimetablesFile:
         json.dump(parse(DOM), rawTimetablesFile, indent=2)
+
+    # ▀▀█▀▀ █▀▀ █▀▄▀█ █▀▀█ █▀▀█ █▀▀█ █▀▀█ █▀▀█ █░░█ 　 ▀▀█▀▀ ░▀░ █▀▄▀█ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀▄ █░░ █▀▀ █▀▀
+    # ░░█░░ █▀▀ █░▀░█ █░░█ █░░█ █▄▄▀ █▄▄█ █▄▄▀ █▄▄█ 　 ░░█░░ ▀█▀ █░▀░█ █▀▀ ░░█░░ █▄▄█ █▀▀▄ █░░ █▀▀ ▀▀█
+    # ░░▀░░ ▀▀▀ ▀░░░▀ █▀▀▀ ▀▀▀▀ ▀░▀▀ ▀░░▀ ▀░▀▀ ▄▄▄█ 　 ░░▀░░ ▀▀▀ ▀░░░▀ ▀▀▀ ░░▀░░ ▀░░▀ ▀▀▀░ ▀▀▀ ▀▀▀ ▀▀▀
+
+    currentDate = datetime.now()
+
+    if (degree_programme_code == "29932") and (currentDate <= datetime(2024, 9, 25)):
+        aula_iv = "Aula IV Matematica G. Castelnuovo (CU006)"
+        aula_v  = "Aula V Matematica G. Castelnuovo (CU006)"
+        matematica_building = "https://maps.app.goo.gl/oU37nArvFccRYNvQ7"
+
+        # 1047622 - CRYPTOGRAPHY
+        course_timetables_dict["1047622"]["channels"]["0"]["martedì"][0]["classrooms"] = {}
+        course_timetables_dict["1047622"]["channels"]["0"]["martedì"][0]["classroomInfo"] = aula_iv
+        course_timetables_dict["1047622"]["channels"]["0"]["martedì"][0]["classroomUrl"] = matematica_building
+
+        # 10596281 - AUTONOMOUS NETWORKING
+        course_timetables_dict["10596281"]["channels"]["0"]["venerdì"][0]["classrooms"] = {}
+        course_timetables_dict["10596281"]["channels"]["0"]["venerdì"][0]["classroomInfo"] = aula_iv
+        course_timetables_dict["10596281"]["channels"]["0"]["venerdì"][0]["classroomUrl"] = matematica_building
+
+        # 10600490 - BLOCKCHAIN AND DISTRIBUTED LEDGER TECHNOLOGIES
+        course_timetables_dict["10600490"]["channels"]["0"]["martedì"][0]["classrooms"] = {}
+        course_timetables_dict["10600490"]["channels"]["0"]["martedì"][0]["classroomInfo"] = aula_iv
+        course_timetables_dict["10600490"]["channels"]["0"]["martedì"][0]["classroomUrl"] = matematica_building
+
+        # 1047642 - SECURITY IN SOFTWARE APPLICATIONS
+        course_timetables_dict["1047642"]["channels"]["0"]["mercoledì"][0]["classrooms"] = {}
+        course_timetables_dict["1047642"]["channels"]["0"]["mercoledì"][0]["classroomInfo"] = aula_v
+        course_timetables_dict["1047642"]["channels"]["0"]["mercoledì"][0]["classroomUrl"] = matematica_building
+
+        # 1041764 - BIG DATA COMPUTING
+        course_timetables_dict["1041764"]["channels"]["0"]["mercoledì"][0]["classrooms"] = {}
+        course_timetables_dict["1041764"]["channels"]["0"]["mercoledì"][0]["classroomInfo"] = aula_v
+        course_timetables_dict["1041764"]["channels"]["0"]["mercoledì"][0]["classroomUrl"] = matematica_building
+
+        # 1047624 - DISTRIBUTED SYSTEMS
+        course_timetables_dict["1047624"]["channels"]["0"]["giovedì"][0]["classrooms"] = {}
+        course_timetables_dict["1047624"]["channels"]["0"]["giovedì"][0]["classroomInfo"] = "Aula B Scienze Biochimiche (CU010)"
+        course_timetables_dict["1047624"]["channels"]["0"]["giovedì"][0]["classroomUrl"] = "https://maps.app.goo.gl/FDurWQ4cwoQVqCn5A"
+
+        # 10589621 - ADVANCED MACHINE LEARNING
+        course_timetables_dict["10589621"]["channels"]["0"].pop("giovedì")
+
+        # 10600495 - AUTOMATIC VERIFICATION OF INTELLIGENT SYSTEMS
+        course_timetables_dict["10600495"]["channels"]["0"]["lunedì"] = [
+          {
+            "teacher": "2bf66397-ce7b-43e0-b640-ca1e45805df4",
+            "timeslot": "14 - 16",
+            "classrooms": {
+              "3204f38e-7393-4457-a108-c048458d026a": "Aula S1 (Edificio: RM113)"
+            }
+          }
+        ]
+
+        # 1047627 - FOUNDATIONS OF DATA SCIENCE
+        course_timetables_dict["1047627"] = {
+            "subject": "FOUNDATIONS OF DATA SCIENCE",
+            "degree": "29932",
+            "channels": {
+              "0": {
+                "luned\u00ec": [
+                  {
+                    "teacher": "c6ebe64b-d218-4bed-9643-8de250010478",
+                    "timeslot": "16 - 19",
+                    "classroomInfo": "Aula 202 Marco Polo (RM021)",
+                    "classroomUrl": "https://maps.app.goo.gl/sLaCKDv3xDLTGDWc7"
+                  }
+                ],
+                "venerd\u00ec": [
+                  {
+                    "teacher": "c6ebe64b-d218-4bed-9643-8de250010478",
+                    "timeslot": "10 - 12",
+                    "classrooms": {
+                      "3247d3bb-417e-4bba-8e7e-829bbb3863de": "Aula 2 (Edificio: RM018)"
+                    }
+                  }
+                ]
+              }
+            },
+            "code": "1047627"
+        }
+
+    #
+    # ▒█▀▀▀ ▀▄▒▄▀ ▒█▀▀█ ▒█▀▀▀█ ▒█▀▀█ ▀▀█▀▀ 　 ▒█▀▀▄ ░█▀▀█ ▀▀█▀▀ ░█▀▀█
+    # ▒█▀▀▀ ░▒█░░ ▒█▄▄█ ▒█░░▒█ ▒█▄▄▀ ░▒█░░ 　 ▒█░▒█ ▒█▄▄█ ░▒█░░ ▒█▄▄█
+    # ▒█▄▄▄ ▄▀▒▀▄ ▒█░░░ ▒█▄▄▄█ ▒█░▒█ ░▒█░░ 　 ▒█▄▄▀ ▒█░▒█ ░▒█░░ ▒█░▒█
+    #
 
     # Save the classroom information to a JSON file
     with open(f"../data/classrooms.json", 'w') as classroomsFile:
