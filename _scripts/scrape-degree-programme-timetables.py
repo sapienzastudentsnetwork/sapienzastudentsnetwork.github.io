54a7b8ec43f54e604857c8e77468de935b4926db
matypist <69389461+matypist@users.noreply.github.com>
2025-10-04 00:14:31 +0200

    No longer hard-code Aula 3L for the Wednesday lesson of 1047642 - SECURITY IN SOFTWARE APPLICATIONS

--- _scripts/scrape-degree-programme-timetables.py
+++ _scripts/scrape-degree-programme-timetables.py
@@ -579,11 +579,6 @@ if __name__ == '__main__':
                         #if day_name == "venerdì" and "classrooms" in day_schedule and "41f8d660-fcfd-4b27-9dc6-8da0e075088b" in day_schedule["classrooms"]:
                         #    day_schedule["classrooms"].pop("41f8d660-fcfd-4b27-9dc6-8da0e075088b")
                         #    day_schedule["classrooms"]["27a4966a-0abc-418d-aa04-ea3973e3cdef"] = "Aula 3 (Edificio: RM018)"
-                    # 1047642 - SECURITY IN SOFTWARE APPLICATIONS
-                    elif course_code == "1047642":
-                        if day_name == "mercoledì" and "classrooms" in day_schedule and "41f8d660-fcfd-4b27-9dc6-8da0e075088b" in day_schedule["classrooms"]:
-                            day_schedule["classrooms"].pop("41f8d660-fcfd-4b27-9dc6-8da0e075088b")
-                            day_schedule["classrooms"]["27a4966a-0abc-418d-aa04-ea3973e3cdef"] = "Aula 3 (Edificio: RM018)"
                     # 1015886 - ALGEBRA
                     elif course_code == "1015886" and channel_id == "1" and "teachers" in day_schedule:
                         day_schedule["teachers"]["c9a7e4f6-798f-4f17-a5a3-93e09d0b1817"] = "DE SOLE ALBERTO"
