72611f7a19bbf54bcb95178ff6528052e97f30d4
matypist <69389461+matypist@users.noreply.github.com>
2026-02-21 20:48:29 +0100

    scrape-degree-programme-timetables.py: implement "change_classrooms"

--- _scripts/scrape-degree-programme-timetables.py
+++ _scripts/scrape-degree-programme-timetables.py
@@ -427,6 +427,17 @@ def apply_manual_overrides(course_timetables_dict, degree_programme_code):
                     if day not in course_timetables_dict[course_code]["channels"][channel]:
                         course_timetables_dict[course_code]["channels"][channel][day] = schedules
 
+    # Override classrooms for specific days without altering teachers or timeslots
+    if "change_classrooms" in overrides:
+        for course_code, course_data in overrides["change_classrooms"].items():
+            if course_code in course_timetables_dict:
+                for channel, channel_data in course_data.get("channels", {}).items():
+                    if channel in course_timetables_dict[course_code]["channels"]:
+                        for day, new_classrooms in channel_data.items():
+                            if day in course_timetables_dict[course_code]["channels"][channel]:
+                                for schedule in course_timetables_dict[course_code]["channels"][channel][day]:
+                                    schedule["classrooms"] = new_classrooms
+
     # Iterate through data for inline updates (teachers and classrooms)
     master_degrees = ("33508", "33516")
     add_teachers = overrides.get("add_teachers", {})
